33f66991630a1fe76f42da00c29be494ba19cd4e
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2024-06-25 12:25:39 +0800

    Add custom declaration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,13 @@
 import express, { Express } from 'express';
 import dateRoutes from './routes/dateRoutes';
 import authRoutes from './routes/authRoutes';
+import { JwtPayload } from 'jsonwebtoken';
+
+declare module "express-serve-static-core" {
+    interface Request {
+        user?: string | JwtPayload;
+    }
+}
 
 const app: Express = express();
 
